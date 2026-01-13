07ae1e9e10b60a663f024b2ef7fe8a1afccf7b9d
Koushik Ghosh <55987395+koushikghosh11@users.noreply.github.com>
2026-01-13 10:21:33 +0530

    Use OpenRouter API

--- research_agent/config.py
+++ research_agent/config.py
@@ -1,7 +1,7 @@
-import os
+from os import getenv
 
-GEMINI_MODEL = "gemini-2.5-flash"
-GEMINI_TEMP = 0.2
-GEMINI_API_KEY = os.environ.get("GOOGLE_API_KEY")
+LLM_MODEL = "openai/gpt-oss-120b:free"
+LLM_TEMP = 0.2
+OPENROUTER_API_KEY = getenv("OPENROUTER_API_KEY")
 
 MAX_ITERATIONS = 3
--- research_agent/llm.py
+++ research_agent/llm.py
@@ -1,14 +1,15 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
-from research_agent.config import GEMINI_MODEL, GEMINI_TEMP, GEMINI_API_KEY
+from langchain_openai import ChatOpenAI
+from research_agent.config import LLM_MODEL, LLM_TEMP, OPENROUTER_API_KEY
 
 
 llm = None
 
 try:
-    llm = ChatGoogleGenerativeAI(
-        model=GEMINI_MODEL,
-        temperature=GEMINI_TEMP,
-        api_key=GEMINI_API_KEY
+    llm = ChatOpenAI(
+        base_url="https://openrouter.ai/api/v1",  # OpenRouter base URL
+        model=LLM_MODEL,
+        temperature=LLM_TEMP,
+        api_key=OPENROUTER_API_KEY
     )
 except Exception as e:
-    raise ValueError(f"❌ Failed to initialize Gemini LLM: {e}")
+    raise ValueError(f"❌ Failed to initialize LLM: {e}")
